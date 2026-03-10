7584de52d2a8396dae643fa1b3dd8b6b452d4d2d
Lutz Ballaschke <snert@tastybyte.de>
2026-03-10 18:57:48 +0100

    test_7seg function

--- src/7seg.c
+++ src/7seg.c
@@ -20,7 +20,7 @@
 #include "7seg.h"
 
 /*
- * 
+ *
  * 7-segment LED display driver.
  * works like gpio but everythin inverted
  * because the circuit has common +5V
@@ -33,13 +33,60 @@ inline void set_7seg_pin(pinconf_t *gpio)
     *gpio->port &= ~(1 << gpio->pin);   /* inverted pgio */
 }
 
+
 inline void reset_7seg_pin(pinconf_t *gpio)
 {
     *gpio->port |= (1 << gpio->pin);    /* inverted pgio */
 }
 
-void reset_7seg_pins(pinconf_t *gpio)
+
+void reset_7seg_pins(pinconf_t *base_addr, uint8_t pin_count)
 {
-    for(int j = 0; j < ADDRCOUNT; j++)
-        reset_7seg_pin(addrpins + j);
+    for(int i = 0; i < pin_count; i++)
+        reset_7seg_pin(base_addr + i);
 }
+
+
+void set_7seg_digit(uint8_t digit)
+{
+    reset_7seg_pins(outpins, PINCOUNT);
+
+    switch(digit)
+    {
+        case 0:
+            set_7seg_pin(outpins + 1);
+            set_7seg_pin(outpins + 2);
+            set_7seg_pin(outpins + 3);
+            set_7seg_pin(outpins + 4);
+            set_7seg_pin(outpins + 5);
+            set_7seg_pin(outpins + 6);
+            set_7seg_pin(outpins + 7);
+        case 1:
+            set_7seg_pin(outpins + 1);
+            set_7seg_pin(outpins + 2);
+        case 2:
+            set_7seg_pin(outpins + 1);
+        case 3:
+            set_7seg_pin(outpins + 1);
+        case 4:
+            set_7seg_pin(outpins + 1);
+        case 5:
+            set_7seg_pin(outpins + 1);
+        case 6:
+            set_7seg_pin(outpins + 1);
+        case 7:
+            set_7seg_pin(outpins + 1);
+        case 8:
+            set_7seg_pin(outpins + 1);
+        case 9:
+            set_7seg_pin(outpins + 1);
+    }
+}
+
+
+void set_7seg_4digit_number(uint16_t number)
+{
+    reset_7seg_pins(outpins, PINCOUNT);
+}
+
+
